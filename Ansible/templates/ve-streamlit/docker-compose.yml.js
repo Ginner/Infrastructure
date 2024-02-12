f68c267f49e5ba4598d536816ed3eb6fe388188c
Ginner <26798615+Ginner@users.noreply.github.com>
2024-02-12 10:14:55 +0100

    Implemented netdata for monitoring

--- Ansible/templates/ve-streamlit/docker-compose.yml.js
+++ Ansible/templates/ve-streamlit/docker-compose.yml.js
@@ -1,4 +1,4 @@
-version: '3.8'
+version: '3'
 
 
 services:
@@ -22,6 +22,34 @@ services:
       - caddy_data:/data
       - caddy_config:/config
 
+  netdata:
+    image: netdata/netdata
+    container_name: netdata
+    hostname: {{ domain_name }}
+    pid: host
+    restart: unless-stopped
+    cap_add:
+      - SYS_PTRACE
+      - SYS_ADMIN
+    security_opt:
+      - apparmor:unconfined
+    volumes:
+      - netdataconfig:/etc/netdata
+      - netdatalib:/var/lib/netdata
+      - netdatacache:/var/cache/netdata
+      - /etc/passwd:/host/etc/passwd:ro
+      - /etc/group:/host/etc/group:ro
+      - /etc/localtime:/etc/localtime:ro
+      - /proc:/host/proc:ro
+      - /sys:/host/sys:ro
+      - /etc/os-release:/host/etc/os-release:ro
+      - /var/log:/host/var/log:ro
+      - /var/run/docker.sock:/var/run/docker.sock:ro
+
 volumes:
   caddy_data:
   caddy_config:
+  netdataconfig:
+  netdatalib:
+  netdatacache:
+
