f724adc4e50e2bc71c1d30300971fff051baa3bb
Morten <morten@valueengineering.dk>
2024-05-23 13:13:34 +0200

    Initial attempt at serving multiple apps

--- Ansible/templates/ve-streamlit/Caddyfile.js
+++ Ansible/templates/ve-streamlit/Caddyfile.js
@@ -1,8 +1,10 @@
-{{ app_subdomain }}.{{ domain_name }} {
-    reverse_proxy {{ app_name }}:8501
+{% for app in apps %}
+{{ app_subdomain }}.{{ domain_name }}/{{ app.name | lower}} {
+    reverse_proxy {{ app.name }}:{{ app.port }}
 }
+{% endfor %}
 
-netdata.{{ domain_name }} {
+{{ app_subdomain }}.{{ domain_name }}/netdata {
     reverse_proxy netdata:19999
     basicauth {
         {{ app_admin_user }} {{ app_admin_password | password_hash('bcrypt') }}
