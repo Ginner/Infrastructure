6249af20683d224e7ccdc324f370a2701f1d4818
Ginner <26798615+Ginner@users.noreply.github.com>
2025-04-07 16:05:33 +0200

    Working jupyter Lab setup

--- NixOS/GLaDOS/containers/jupyter.nix
+++ NixOS/GLaDOS/containers/jupyter.nix
@@ -2,24 +2,39 @@
   image = "quay.io/jupyter/scipy-notebook:latest";
 
   environment = {
-    JUPYTER_ENABLE_LAB = "yes";  # Ensures JupyterLab is the default interface
+    # JUPYTER_ENABLE_LAB = "yes";  # Ensures JupyterLab is the default interface
+    # JUPYTER_TOKEN = "";  # Empty token allows access without authentication
+    # JUPYTER_CONFIG_DIR = "/home/jovyan/.jupyter";
     NB_UID = "1000";  # User ID for the notebook user
     NB_GID = "100";   # Group ID for the notebook user
   };
 
   autoStart = true;
 
-  ports = [
-    "8888:8888"  # Map container port 8888 to host port 8888
+  volumes = [
+    "/home/ginner/notebooks:/home/jovyan/work"
+    "/etc/nixos/containers/jupyter/config:/home/jovyan/.jupyter"  # For persistent configuration
   ];
 
-  volumes = [
-    "/home/ginner/notebooks:/home/jovyan/work"  # Replace with your desired path to store notebooks
+  cmd = [
+    "start.sh"
+    "jupyter"
+    "lab"
+    "--LabApp.ip='0.0.0.0'"
+    "--LabApp.open_browser=false"
+    "--LabApp.allow_origin='*'"
+    "--LabApp.token=''"
+    # "--ServerApp.allow_origin='*'"
+    # "--ServerApp.allow_remote_access=true"
+    # "--ServerApp.password_required=false"
+    # # "--NotebookApp.base_url='/jupyter'"
+    # "--NotebookApp.token=''"
+    # "--NotebookApp.ip='0.0.0.0'"
   ];
 
   extraOptions = [
     "--pull=newer"
-    "--name=jupyterlab"
+    "--name=jupyter"
     "--network=pod-net"
   ];
 }
